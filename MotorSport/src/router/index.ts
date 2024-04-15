5a006077a6abf36ca5f1d4d879da9a016965982b
Yesenia <yesenia.jimenez.poot@gmail.com>
2024-04-15 17:46:27 -0500

    Comprobar el estado de autenticación en cada ruta

--- MotorSport/src/router/index.ts
+++ MotorSport/src/router/index.ts
@@ -1,5 +1,5 @@
 // router/index.ts
-
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { createRouter, createWebHistory } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 import Home from '@/views/Home.vue';
@@ -41,6 +41,23 @@ const router = createRouter({
   // Usa import.meta.env.VITE_BASE_URL en lugar de process.env.BASE_URL
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes,
+});
+
+
+// Crear instancia de autenticación
+const auth = getAuth();
+
+router.beforeEach((to, from, next) => {
+  // Comprobar el estado de autenticación en cada cambio de ruta
+  onAuthStateChanged(auth, (user) => {
+    if (user && (to.name === 'Login' || to.name === 'Register')) {
+      // Si el usuario está autenticado y la ruta es login o register, redirige a Home
+      next({ name: 'Home' });
+    } else {
+      // En cualquier otro caso, sigue con la navegación prevista
+      next();
+    }
+  });
 });
 
 export default router;
